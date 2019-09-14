15b88fdaf0e5ff45397df2511ce2bed3b64180b5
FERNANDO DO NASCIMENTO VIEIRA <35479927+FernandoNv@users.noreply.github.com>
2019-09-14 19:32:18 -0300

    Testes

--- Blog - Exercicio/Main.java
+++ Blog - Exercicio/Main.java
@@ -1,17 +1,34 @@
+import Entidades.Blog;
+import Entidades.Conteudo;
+import Entidades.Nota;
 import Entidades.Usuario;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        /*
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nome: ");
         String nome = scanner.nextLine();
         System.out.println("Email: ");
         String email = scanner.nextLine();
+        */
+        Usuario fernando = new Usuario("Fernando do Nascimento", "email@email.com");
+        Blog myBlog = new Blog("Meu Blog", fernando);
 
-        Usuario usuario = new Usuario(nome, email);
-        System.out.println("Nome: " + usuario.getNome());
-        System.out.println("Email: " + usuario.getEmail());
+        Nota post1 = new Nota("Meu primeiro texto. Parece estar funcionando.", fernando);
+        Nota post2 = new Nota("Meu segundo texto. Parece estar funcionando.", fernando);
+        Nota post3 = new Nota("Meu terceiro texto. Parece estar funcionando.", fernando);
+
+        myBlog.criarNota(post1);
+        myBlog.criarNota(post2);
+        myBlog.criarNota(post3);
+
+        Nota[] minhasNotas = (Nota[]) myBlog.lerNotas().clone();
+        for (Nota minhasNota : minhasNotas) {
+            System.out.println(minhasNota.getDataCriacao());
+            System.out.println(minhasNota.getAutor().getNome());
+            System.out.println(minhasNota.getMensagem());
+            System.out.println();
+        }
     }
 }
